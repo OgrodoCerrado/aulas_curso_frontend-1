c3ee06eb7c6ce87dbcb71f3f84f8d4adfb34ed58
OgrodoCerrado <Igorpechir@yahoo.com.br>
2025-05-28 21:42:21 -0300

    Aula de java scrpit

--- script-02.js
+++ script-02.js
@@ -1,6 +1,12 @@
 function soma(){
-  var a = document.getElementById('valor01').value;
-  var b = document.getElementById('valor02').value;
-  document.getElementById('demo').innerHTML = a + b;
-  //document.getElementById('demo').innerHTML = parseInt(a) + parseInt(b);
+  var valor_a = document.getElementById('valor01').value;
+  var valor_b = document.getElementById('valor02').value;
+  //document.getElementById('demo').innerHTML = valor_a + valor_b;
+  document.getElementById('demo').innerHTML = parseInt(valor_a) + parseInt(valor_b);
+}
+function subtracao(){
+  var valor_a = document.getElementById('valor01').value;
+  var valor_b = document.getElementById('valor02').value;
+  //document.getElementById('demo').innerHTML = valor_a + valor_b;
+  document.getElementById('demo').innerHTML = parseInt(valor_a) - parseInt(valor_b);
 }
